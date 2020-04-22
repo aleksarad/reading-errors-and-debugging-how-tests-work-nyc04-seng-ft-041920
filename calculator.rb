e3309d009e4cf3e927213dca8e64615b2fb50764
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-22 14:10:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,6 @@ first_number = 27
 second_number =  9
 sum = first_number + second_number
 difference =  first_number - second_number
+product =  first_number * second_number
+quotient = first_number/second_number
 
